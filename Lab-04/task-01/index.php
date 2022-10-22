2dba07681c78222976ebaa36666c82de5a5f7a8e
Nayeema Hoque <113884690+nayeemahoque@users.noreply.github.com>
2022-10-22 16:37:24 +0600

    Create index.php

---
+++ Lab-04/task-01/index.php
@@ -0,0 +1,26 @@
+<html> 
+    <head> 
+        <title>HTML Form</title>
+        <body> 
+          <table>
+              <td>
+                <fieldset>
+                  <legend>
+                      <h2>LOGIN</h2>
+                  </legend>
+                  <form method="post" action="validation.php" enctype="">
+                       Username: <input type="text" name="username" value=""/><br>
+                       <br>
+                       Password: <input type="password" name="password" value=""/> <br>
+                       <hr>
+                       <input type="checkbox" name="" value="" /> Remember me
+                       <br><br>
+                       <input type="submit" name="submit" value="Submit"/>
+                       <a href="next.html" target="_blank"> Forgot password? </a>
+                  </form>           
+              </fieldset>
+            </td> 
+         </table>   
+        </body>
+    </head>
+</html>
